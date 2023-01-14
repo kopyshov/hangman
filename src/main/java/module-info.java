1d7137330c61cde010f97b1cac8b3cee84c340d5
KhariZZma <dkopysh@gmail.com>
2023-01-14 22:45:35 +0300

    Update pom.xml for Desktop Application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.kopyshov.hangman {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.kopyshov.hangman to javafx.fxml;
     exports com.kopyshov.hangman;
 }
